25668c0b8ef14319cb95abc87337bd4183502016
Arav Upadhyay <89582303+FlaShOPArav@users.noreply.github.com>
2024-09-26 11:15:48 +0530

    Create hello_world.cpp

---
+++ hello_world.cpp
@@ -0,0 +1,15 @@
+//Arav Upadhyay
+//23070123026
+//Aim - Printing out Hello World
+
+#include<iostream>
+using namespace std;
+int main()
+{
+    cout<<"Hello world";
+}
+
+/*
+OUTPUT:
+Hello world
+*/
